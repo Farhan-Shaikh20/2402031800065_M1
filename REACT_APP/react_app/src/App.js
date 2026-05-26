521a594dc242bebe03a13df6e4b6c763fee667d2
Farhan Shaikh <is499321@gmail.com>
2026-05-26 13:04:09 +0530

    update react app and node files

--- REACT_APP/react_app/src/App.js
+++ REACT_APP/react_app/src/App.js
@@ -4,42 +4,38 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Card from "./components/Card";
+import Staets from "./components/Staets";
 
 function App() {
   return (
     <div>
-
       <Navbar />
 
       <Hero />
 
-       <div className="cards-row">
+      <div className="cards-row">
         <Card
           title="HTML"
           description="HTML is use to create webpage structure"
         />
-        <Card
-          title="CSS"
-          description="CSS is use for styling websites"
-        />
+        <Card title="CSS" description="CSS is use for styling websites" />
         <Card
           title="JavaScript"
           description="JS adds functionality, logic, and interaction in website"
         />
-        <Card
-          title="React"
-          description="React builds modern UI"
-        />
+        <Card title="React" description="React builds modern UI" />
         <h6>This part is created using props-propertise</h6>
       </div>
 
       <About />
 
       <Contact />
-     
 
+      <div style={{ textAlign: "center" }}>
+        <h3>Give feedback using states in react</h3>
+        <Staets />
+      </div>
       <Footer />
-
     </div>
   );
 }
--- REACT_APP/react_app/src/components/Hero.jsx
+++ REACT_APP/react_app/src/components/Hero.jsx
@@ -1,10 +1,13 @@
 import "../css/Hero.css";
 function Hero(){
+    function show(){
+        alert("pleas   log in first!!")
+    }
     return(
         <div className="hero">
             <h1> lern react with me</h1>
             <p>We are build modern website for learning full stack</p>
-            <button>Get started</button>
+            <button onClick={show}>Get started</button>
         
         </div>
     );
